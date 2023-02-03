61a97ce954a02a6bbfbaec7604e08f8fb9095afd
Sandeep <sandeepsk15892>
2023-02-03 10:04:33 +0530

    create first get request

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/magicVilla")]
+	[ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Villa> GetVillas()
+		{
+			return new List<Villa>
+			{
+				new Villa {Id=1,Name="Pool View"},
+				new Villa {Id=2,Name="Beach View"},
+			};
+		}
+	}
+}
---
+++ MagicVilla_VillaAPI/Model/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaAPI.Model
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}

8b0309d602a144fdd54eaa6032cf85f0e2561856
Sandeep <sandeepsk15892>
2023-02-03 10:19:21 +0530

    add second get request ,search by id

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,25 @@
-﻿using MagicVilla_VillaAPI.Model;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-	[Route("api/magicVilla")]
+	//[Route("api/[controller]")]
+	[Route("api/VillaAPI")]
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<Villa> GetVillas()
+		public IEnumerable<VillaDTO> GetVillas()
 		{
-			return new List<Villa>
-			{
-				new Villa {Id=1,Name="Pool View"},
-				new Villa {Id=2,Name="Beach View"},
-			};
+			return VillaStore.villaList;
+		}
+
+		[HttpGet("{id:int}")]
+		public VillaDTO GetVilla(int id) 
+		{
+			return VillaStore.villaList.FirstOrDefault(u => u.Id == id);			
 		}
 	}
 }
